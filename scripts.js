a7bc18f56e440f7b2dedfbc1bd1ba5d6f1cb5153
Felipe Albuquerque <rockalbuquerque@gmail.com>
2022-06-13 17:51:12 -0300

    Atualização de cardápio e correção de bugs - Versão Final

--- scripts.js
+++ scripts.js
@@ -2,9 +2,13 @@ let contadorCaixa = 0;
 let precoPrato = 0;
 let precoBebida = 0;
 let precoSobremesa = 0;
+let pratoSelecionado = '';
+let bebidaSelecionada = '';
+let sobremesaSelecionada = '';
 
-function selecionarCaixaPrato(menuCaixa, valor) {
+function selecionarCaixaPrato(menuCaixa, valor, prato) {
     precoPrato = valor;
+    pratoSelecionado = prato;
     const menuCaixaSelecionado = document.querySelector(".selecionado-prato");
     if (menuCaixaSelecionado !== null) {
         menuCaixaSelecionado.classList.remove("selecionado-prato");
@@ -23,8 +27,9 @@ function selecionarIconePrato(menuIcone) {
     menuIcone.classList.add("menu-icone-prato");
 }
 
-function selecionarCaixaBebida(menuCaixa, valor) {
+function selecionarCaixaBebida(menuCaixa, valor, bebida) {
     precoBebida = valor;
+    bebidaSelecionada = bebida;
     const menuCaixaSelecionado = document.querySelector(".selecionado-bebida");
     if (menuCaixaSelecionado !== null) {
         menuCaixaSelecionado.classList.remove("selecionado-bebida");
@@ -43,8 +48,9 @@ function selecionarIconeBebida(menuIcone) {
     menuIcone.classList.add("menu-icone-bebida");
 }
 
-function selecionarCaixaSobremesa(menuCaixa, valor) {
+function selecionarCaixaSobremesa(menuCaixa, valor, sobremesa) {
     precoSobremesa = valor;
+    sobremesaSelecionada = sobremesa;
     const menuCaixaSelecionado = document.querySelector(".selecionado-sobremesa");
     if (menuCaixaSelecionado !== null) {
         menuCaixaSelecionado.classList.remove("selecionado-sobremesa");
@@ -73,7 +79,7 @@ function botaoPedido() {
 
 function fecharPedido() {
     let precoTotal = precoPrato + precoBebida + precoSobremesa;
-    let mensagem = "Olá, gostaria de fazer o pedido: \n\ - Prato: Frango Yin Yang \n\ - Bebida: Coquinha Gelada \n\ - Sobremesa: Pudim \n\ Total: R$ " + precoTotal.toFixed(2);
+    let mensagem = `Olá, gostaria de fazer o pedido: \n\ - Prato: ${pratoSelecionado} \n\ - Bebida: ${bebidaSelecionada} \n\ - Sobremesa: ${sobremesaSelecionada} \n\ Total: R$ ${precoTotal.toFixed(2)}`;
     pedidoLink = "https://wa.me/5521983489668?text=" + encodeURIComponent(mensagem);
     window.open(pedidoLink);
 }
